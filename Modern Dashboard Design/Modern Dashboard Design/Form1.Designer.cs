0d23aabf390a52775839aa77ce0c5a3a91c7fba3
geethsheheran <68048419+GeethSheheran@users.noreply.github.com>
2023-12-30 01:04:00 +0530

    Update Form1.Designer.cs

--- Modern Dashboard Design/Modern Dashboard Design/Form1.Designer.cs
+++ Modern Dashboard Design/Modern Dashboard Design/Form1.Designer.cs
@@ -7,6 +7,7 @@
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
+        
         /// <summary>
         /// Clean up any resources being used.
         /// </summary>
